dd19d2b3443440f27db3abc255f511e806e4e359
Arash <a.hemmati@gmail.com>
2024-04-20 11:08:06 +0330

    in memory is added

--- src/main/java/ir/oraclej/hsqlDemo/HSQLConnection.java
+++ src/main/java/ir/oraclej/hsqlDemo/HSQLConnection.java
@@ -1,11 +1,15 @@
 package ir.oraclej.hsqlDemo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class HSQLConnection {
-    private static String url = "jdbc:hsqldb:file:D:\\hrdb.db";
+//    private static String url = "jdbc:hsqldb:file:D:\\hrdb.db";
+    private static String url = "jdbc:hsqldb:mem:.";
     private static String driver = "org.hsqldb.jdbc.JDBCDriver";
     private static String username = "sa";
     private static String password = "password";
@@ -15,9 +19,20 @@ public class HSQLConnection {
 
     private HSQLConnection(){
         try {
-//            Class.forName(driver);
+            Class.forName(driver);
             connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException  e) {
+            initScript();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initScript() {
+        try {
+            Path p = Path.of(getClass().getClassLoader().getResource("init.sql").toURI());
+            String sql = Files.readString(p);
+            connection.prepareStatement(sql).execute();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
